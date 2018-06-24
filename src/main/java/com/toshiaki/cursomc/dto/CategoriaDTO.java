1c3eecc5e38f0942c0679cc435e98c96a3afc807
Lucas Tsutsumi <lucastsutsumi@gmail.com>
2018-06-24 20:47:36 -0300

    Bean validation em Categoria

--- src/main/java/com/toshiaki/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/toshiaki/cursomc/dto/CategoriaDTO.java
@@ -2,17 +2,25 @@ package com.toshiaki.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.toshiaki.cursomc.domain.Categoria;
 
-public class CategoriaDTO implements Serializable{
+public class CategoriaDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	private Integer id;
-	private String nome;
 	
+	private Integer id;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Length(min = 5, max = 80, message = "Tamanho deve ser entre 5 e 80")
+	private String nome;
+
 	public CategoriaDTO() {
 	}
-	
+
 	public CategoriaDTO(Categoria obj) {
 		this.id = obj.getId();
 		this.nome = obj.getNome();
@@ -33,6 +41,5 @@ public class CategoriaDTO implements Serializable{
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
-	
+
 }
--- src/main/java/com/toshiaki/cursomc/services/CategoriaService.java
+++ src/main/java/com/toshiaki/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.toshiaki.cursomc.domain.Categoria;
+import com.toshiaki.cursomc.dto.CategoriaDTO;
 import com.toshiaki.cursomc.repositories.CategoriaRepository;
 import com.toshiaki.cursomc.services.exceptions.DataIntegrityException;
 import com.toshiaki.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,5 +54,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String direction, String orderBy){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 }
