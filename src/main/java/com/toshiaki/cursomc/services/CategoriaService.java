16da52959f642f9b388d87ded7578a1bfc72b957
Lucas Tsutsumi <lucastsutsumi@gmail.com>
2018-12-15 22:13:43 -0200

    Correcoes: update em CategoriaService e construtor de Pagamento

--- src/main/java/com/toshiaki/cursomc/services/CategoriaService.java
+++ src/main/java/com/toshiaki/cursomc/services/CategoriaService.java
@@ -32,10 +32,10 @@ public class CategoriaService {
 		return repo.save(obj);
 	}
 	
-	public Categoria update(Categoria categoria) {
-		find(categoria.getId());
-		Categoria obj = repo.save(categoria);
-		return obj;
+	public Categoria update(Categoria obj) {
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 	
 	public void delete(Integer id) {
@@ -58,5 +58,9 @@ public class CategoriaService {
 	
 	public Categoria fromDTO(CategoriaDTO objDTO) {
 		return new Categoria(objDTO.getId(), objDTO.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 }
