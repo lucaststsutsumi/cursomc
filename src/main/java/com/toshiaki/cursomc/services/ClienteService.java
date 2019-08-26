081acbcb34461fc14e5d624106b1439b77cc6bfc
Lucas Tsutsumi <lucastsutsumi@gmail.com>
2019-08-26 00:43:29 -0300

    Ajustando e testando a deleção de Cliente

--- src/main/java/com/toshiaki/cursomc/services/ClienteService.java
+++ src/main/java/com/toshiaki/cursomc/services/ClienteService.java
@@ -56,7 +56,7 @@ public class ClienteService {
 		try{
 			repo.deleteById(id);	
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possivel excluir Cliente que possua vinculos");
+			throw new DataIntegrityException("Não é possivel excluir porque há pedidos relacionados");
 		}
 	}
 	
