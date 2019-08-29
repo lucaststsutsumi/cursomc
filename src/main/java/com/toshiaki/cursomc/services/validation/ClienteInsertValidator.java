ef68ea68f1611ffaef1a51acc09845822acf2814
Lucas Tsutsumi <lucastsutsumi@gmail.com>
2019-08-29 20:33:40 -0300

    Validação customizada: email não repitido na inserção de Cliente

--- src/main/java/com/toshiaki/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/toshiaki/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.toshiaki.cursomc.domain.Cliente;
 import com.toshiaki.cursomc.domain.enuns.TipoCliente;
 import com.toshiaki.cursomc.dto.ClienteNewDTO;
+import com.toshiaki.cursomc.repositories.ClienteRepository;
 import com.toshiaki.cursomc.resources.exception.FieldMessage;
 import com.toshiaki.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Autowired
+	private ClienteRepository clienteRepository;
+
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -20,14 +27,21 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<>();
 
+		// validação CPF
 		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
 		}
 
+		// validação CNPJ
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
-		// inclua os testes aqui, inserindo erros na lista
+
+		// validação EMAIL UNICO
+		Cliente cliente = clienteRepository.findByEmail(objDto.getEmail());
+		if (cliente != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
