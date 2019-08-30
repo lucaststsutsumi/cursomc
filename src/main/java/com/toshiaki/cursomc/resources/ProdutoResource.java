f9c759e64bd92a91f2df7dddb18e50b2108d8606
Lucas Tsutsumi <lucastsutsumi@gmail.com>
2019-08-30 02:29:44 -0300

    busca de produtos por nome e categorias

---
+++ src/main/java/com/toshiaki/cursomc/resources/ProdutoResource.java
@@ -0,0 +1,45 @@
+package com.toshiaki.cursomc.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.toshiaki.cursomc.domain.Produto;
+import com.toshiaki.cursomc.dto.ProdutoDTO;
+import com.toshiaki.cursomc.resources.utils.URL;
+import com.toshiaki.cursomc.services.ProdutoService;
+
+@RestController
+@RequestMapping(value = "/produtos")
+public class ProdutoResource {
+
+	@Autowired
+	private ProdutoService service;
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Produto> find(@PathVariable Integer id) {
+		Produto obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<Page<ProdutoDTO>> findPage(@RequestParam(value = "nome", defaultValue = "") String nome,
+			@RequestParam(value = "categorias", defaultValue = "0") String categorias,
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy) {
+		String nomeDecoded = URL.decodeParam(nome);
+		List<Integer> ids = URL.decodeInList(categorias);
+		Page<Produto> list = service.search(nomeDecoded, ids, page, linesPerPage, direction, orderBy);
+		Page<ProdutoDTO> listDTO = list.map(obj -> new ProdutoDTO(obj));
+		return ResponseEntity.ok().body(listDTO);
+	}
+}
---
+++ src/main/java/com/toshiaki/cursomc/resources/utils/URL.java
@@ -0,0 +1,30 @@
+package com.toshiaki.cursomc.resources.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.List;
+
+public class URL {
+	public static String decodeParam(String s) {
+		try {
+			return URLDecoder.decode(s, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "";
+		}
+	}
+
+	public static List<Integer> decodeInList(String s) {
+		String[] vet = s.split(",");
+		List<Integer> list = new ArrayList<>();
+
+		for (int i = 0; i < vet.length; i++) {
+			list.add(Integer.parseInt(vet[i]));
+		}
+		return list;
+		// return Arrays.asList(s.split(",")).stream().map(x ->
+		// Integer.parseInt(x)).collect(Collectors.toList());
+	}
+}
